e2e7cda3a5735564e726a70bf107644c8bec1837
rafaelsoares07 <rafaelsoares1936@gmail.com>
2023-02-25 22:52:56 -0300

    feat: :sparkles: create routes using miragiJS to simulate an api

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+createServer({
+
+  models: {
+    transaction: Model,
+  },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Frelancer',
+          type: 'deposit',
+          category: 'Dev',
+          amount: 4000,
+          createAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 2,
+          title: 'Roupas Novas',
+          type: 'deposit',
+          category: 'Extras',
+          amount: 150,
+          createAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 3,
+          title: 'Roupas Novas',
+          type: 'deposit',
+          category: 'Extras',
+          amount: 150,
+          createAt: new Date('2021-02-12 09:00:00')
+        }, {
+          id: 4,
+          title: 'Roupas Novas',
+          type: 'withdraw',
+          category: 'Extras',
+          amount: 150,
+          createAt: new Date('2021-02-12 09:00:00')
+        },
+        {
+          id: 5,
+          title: 'Roupas Novas',
+          type: 'withdraw',
+          category: 'Extras',
+          amount: 15000,
+          createAt: new Date('2021-02-12 09:00:00')
+        }
+      ]
+    })
+  },
+  routes() {
+    this.namespace = 'api'
+
+    this.get('/transactions', () => {
+      return this.schema.all('transaction')
+    })
+
+    this.post('/transaction', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data)
+    })
+  },
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
